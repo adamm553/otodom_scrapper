b6bb56780136cc28c11ff78eb48f68309c93c7a7
Adam Piątek <adam@mac.home>
2026-01-13 19:10:51 +0100

    readme added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from scrapper import Scrapper
-from config import BASE_URL, HEADERS, START_PAGE, END_PAGE
+from config import BASE_URL, HEADERS, START_PAGE, END_PAGE, FILE_NAME
 
 if __name__ == "__main__":
     
 
     scraper = Scrapper(BASE_URL, HEADERS)
     scraper.run(START_PAGE, END_PAGE) 
-    scraper.to_csv("Mieszkania_OOP.csv") 
+    scraper.to_csv(FILE_NAME) 
